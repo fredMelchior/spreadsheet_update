e611482a8806525110bb8904bcd2ee80e19cdaff
Fred Melchior <fred@MacBook-Air-de-Fred.local>
2024-01-31 18:54:18 -0300

    Python app for spreadsheet update

---
+++ main.py
@@ -0,0 +1,101 @@
+import gspread
+
+
+def google_sheets_auth():
+    """
+    gspread Authentication with json file
+    getting spreadsheet data by URL
+    :return: spreadsheet as sh
+    """
+    gc = gspread.service_account(
+        filename="/Users/fred/PycharmProjects/tunts-test-project/norse-voice-400513"
+                 "-d0840a6c0d47"
+                 ".json"
+    )
+    sh = gc.open_by_url(
+        "https://docs.google.com/spreadsheets/d/15FwTx56cG-Zp_yoyNfEupes8IAZ4zevOzO0_Ncb3XT8/edit?usp"
+        "=sharing"
+    )
+
+    return sh
+
+
+def spreadsheet_data(sheet):
+    """
+    :param sheet:
+    :return: spreadsheet data without the headers
+    """
+    return sheet.get_all_values()[3:]
+
+
+def average_calculator(line_data, columns):
+    """
+    Calculate the grade point average of each student
+    param: line_data, columns
+    return: grade point average
+    """
+
+    grades = []
+
+    for test in ["P1", "P2", "P3"]:
+        try:
+            grades.append(float(line_data[columns[test]]))
+        except (KeyError, IndexError, ValueError):
+            pass
+
+    if grades:
+        return sum(grades) / len(grades)
+    else:
+        return None
+
+
+def situation_and_naf(average, absences, total_classes):
+    """
+    Function to set the student situation and points to be approved with a final test
+    :param average:
+    :param absences:
+    :param total_classes:
+    :return: situation and NAF
+    """
+    if 50 <= average < 70:
+        return "Exame Final", round(max(0, 150 - average), 1)
+    # elif 50 <= average < 70:
+    #     return "Exame Final", max(0, round((14 - average) * 2))
+    elif average < 50:
+        return "Reprovado por Nota", 0
+    elif absences > 0.25 * total_classes:
+        return "Reprovado por Falta", 0
+    else:
+        return "Aprovado", 0
+
+
+def update_spreadsheet(sheet, data, columns):
+    """
+    Final function that uses all information to update the spreadsheet with the new data
+    :param sheet:
+    :param data:
+    :param columns:
+    :return: NAF and Situation columns updated
+    """
+    for i, line_data in enumerate(data, start=4):
+        average = round(average_calculator(line_data, columns), 1)
+        absences = int(line_data[columns["Faltas"]])
+        situation, naf = situation_and_naf(average, absences, total_classes=60)
+
+        sheet.update_acell(f"G{i}", situation)
+        sheet.update_acell(f"H{i}", str(naf))
+
+
+def main():
+    spreadsheet = google_sheets_auth()
+    sheet = spreadsheet.get_worksheet(0)
+
+    columns = {"Matricula": 0, "Nome": 1, "Faltas": 2, "P1": 3, "P2": 4, "P3": 5}
+
+    data = spreadsheet_data(sheet)
+
+    update_spreadsheet(sheet, data, columns)
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Desafio de Programção - Tunts.Rocks
+
+Esta aplicação em Python foi desenvolvida para cumprir o desafio de implementar
+lógicas em uma planilha Google para determinação da situação de alunos baseada
+em sua nota média e também a nota final necessária para a aprovação em exame final.
+
+## Como utilizar
+
+No terminal ou IDE de sua preferência, acesse a pasta da aplicação,
+então utilize os seguintes comandos:
+
+### Ative o ambiente virtual (opcional):
+
+source venv/bin/activate
+
+### Instale as bibliotecas:
+
+pip install --upgrade pip
+
+pip install requirements.txt
+
+### Rode a aplicação utilizando a IDE ou no terminal com:
+
+python main.py
+
+### A aplicação irá fazer o update automático na planilha!😎
